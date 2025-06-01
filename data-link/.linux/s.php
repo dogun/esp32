94171c78dfe38ecd6002296ba329c3763b8ece0b
dogun <yuexuqiang@gmail.com>
2025-06-01 21:58:31 +0800

    u

--- data-link/.linux/s.php
+++ data-link/.linux/s.php
@@ -36,6 +36,7 @@ foreach ($s as $sn) {
 }
 
 $sql = "select timestamp, type $ss from sensor where timestamp >= $stime and timestamp <= $etime and board_no=$b order by timestamp asc";
+echo $sql;
 $q = $mysqli->query($sql);
 $data = array();
 while (($row = $q->fetch_assoc()) != NULL) {
